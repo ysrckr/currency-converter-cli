3ec84575e2383c45e440a372836dbd88e43177fd
Yasar Cakir <dev.yasarcakir@gmail.com>
2024-08-06 20:29:52 +0700

    Replace Bubbletea with Huh package

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"log"
 
-	bubble "github.com/charmbracelet/bubbletea"
-	"github.com/ysrckr/currency-converter-cli/internal/tea"
+	"github.com/ysrckr/currency-converter-cli/internal/huh"
 )
 
 func main() {
-	p := bubble.NewProgram(tea.InitialModel())
-	if _, err := p.Run(); err != nil {
-		fmt.Printf("Alas, there's been an error: %v", err)
-		os.Exit(1)
+	err := huh.Form.Run()
+	if err != nil {
+		log.Fatal(err)
 	}
+
 }
