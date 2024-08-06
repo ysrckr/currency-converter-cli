d6f7afa0bb5c31aa60efb06264b1f507c2e2287f
Yasar Cakir <dev.yasarcakir@gmail.com>
2024-08-06 18:45:24 +0700

    Add Bubble Tea CLI and initial model

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	bubble "github.com/charmbracelet/bubbletea"
+	"github.com/ysrckr/currency-converter-cli/internal/tea"
+)
+
 func main() {
-	
+	p := bubble.NewProgram(tea.InitialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
 }
